3f783ea8c59288d8b76a55ffacf2f9d04a83ef30
BJ <26101019+BraveNewWord@users.noreply.github.com>
2022-08-06 01:45:24 +1200

    Add word search bar

--- frontend-module/my-app/src/App.tsx
+++ frontend-module/my-app/src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [searchWord, setSearchWord] = useState("");
+
   return (
     <div>
-        <p>
-            Hello, world
-        </p>
+      <h1>
+        Word Search
+      </h1>
+
+      <div>
+        <label>Word</label><br/>
+        <input type="text" id="word" name="word" onChange={e => setSearchWord(e.target.value)}/><br/>
+        <button onClick={search}>
+        Search
+        </button>
+      </div>
+
+      <p>
+        You have entered {searchWord}
+      </p>
+
     </div>
   );
+
+  function search(){
+    console.log(searchWord);
+}
 }
 
 export default App;
